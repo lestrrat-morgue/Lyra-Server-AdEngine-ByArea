ebba15ee2b9f403a7b3481df833ffa67a054505e
lestrrat <lestrrat+github@gmail.com>
2010-04-07 12:11:41 +0900

    add template

---
+++ templates/default.js
@@ -0,0 +1,8 @@
+document.writeln('<ul>');
+? my $uri = shift;
+? for my $ad (@_) { # id, title, content
+?   $uri->query_form( rid => $ad->[3], ad => $ad->[0] );
+document.writeln('<li><a href="<?= $uri ?>" target="_blank"><?= $ad->[1] ?></a> <?= $ad->[2] ?></li>');
+? }
+document.writeln('</ul>');
+
